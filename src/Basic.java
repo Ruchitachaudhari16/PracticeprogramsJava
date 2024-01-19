a7f002cdc728e231d050b619781cce98bb6f12b6
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-01-19 14:12:35 +0530

    java basic programs

---
+++ src/Basic.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+public class Basic {
+    /*Print msg*/
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java");
+    }
+    //class is keyword.
+    //public:- is a access modifer
+    //static:- is keyword
+    //void:- is return type
+    //Main is starting point of program
+    //(String[] args):-it will accept the command line  arguments
+}
